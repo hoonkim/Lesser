d7ef6142f97fc548b301efa69db424a0d3aa88c2
GilHonghee <rlfghdgml@naver.com>
2015-10-25 17:56:18 +0900

    fix bug

--- bridge/tests/test_bridge.py
+++ bridge/tests/test_bridge.py
@@ -1,19 +1,13 @@
-﻿import sys
-from bridge.bridge import Bridge
+﻿from bridge.bridge import Bridge
 from unittest import TestCase
+
 
 __author__ = 'honghee'
 
 
-class Machine():
-    def __init__(self, addr, uuid, port):
-        self.addr = addr
-        self.uuid = uuid
-        self.port = port
-
 class TestBridge(TestCase):
-    def test_dummy(self):
 
+    def test_dummy(self):
         bridge = Bridge()
-        bridge.application('App1').Schema('JoinPage').find({"dd":"dd"})
-        bridge.application('App1').Schema('JoinPage').find({"dd":"dd"},{"ddd":0,"dddd":0})
+        print("ddd")
+        bridge.application('App1').Schema('JoinPage').find({"id":"sf","pw":"WJ"})
