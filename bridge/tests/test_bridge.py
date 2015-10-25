4f72a6a15102ceda62ff25f01780003230d87638
GilHonghee <rlfghdgml@naver.com>
2015-10-25 17:15:27 +0900

    test2

--- bridge/tests/test_bridge.py
+++ bridge/tests/test_bridge.py
@@ -2,12 +2,15 @@
 from bridge.bridge import Bridge
 from unittest import TestCase
 
-
 __author__ = 'honghee'
 
 
 class TestBridge(TestCase):
-
     def test_dummy(self):
+        queryList = [
+            {"t1": "v1", "t2": "v2"}
+            , {"_id": 0}
+        ]
+
         bridge = Bridge()
-        bridge.application('App1').Schema('JoinPage').find({"id":"sf","pw":"WJ"}); 
+        bridge.application('App1').Schema('JoinPage').find(queryList)
