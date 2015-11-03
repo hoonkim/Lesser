3ff0d0741151440a4748c71143d0555547a313de
chuckkim <kimmancj2@hotmail.com>
2015-11-04 00:52:06 +0900

    Merge branch '0.0.2' of https://github.com/hoonkim/Lesser
    # Conflicts:
    #	dockermanager/controller.py

--- dockermanager/configure.py
+++ dockermanager/configure.py
@@ -1,7 +1,7 @@
 __author__ = 'dejawa'
 
-host = 'inputServerIP' #server ip
-port = 'inputServerDockerPort'           #server docker port
+host = '175.126.105.125' #server ip
+port = '22375'           #server docker port
 
 
 clientPort = '80'     #example of lesser client port in docker image
@@ -10,7 +10,7 @@ mongoPort = '27017'   #example of mongo port in docker image
 path = '/'  #disk, mem info location path
 
 diskLimit = 3 * 1000 * 1024 * 1024 # 3GB m
-MemLimit = 1 * 1000 *1024 * 1024 # 1GB
+memLimit = 1 * 1000 *1024 * 1024 # 1GB
 
 configObj = {
     "image":"dejawa/lesser",
